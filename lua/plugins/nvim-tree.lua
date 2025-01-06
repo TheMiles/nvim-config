36529c8d0005b6f40ea1dbb1289171642d2597ec
TheMiles <miles@coders-of-the-caribbean.org>
2025-01-06 16:05:29 +0100

    Move nvim-tree config functions to config section

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,55 +6,6 @@
 -----------------------------------------------------------
 
 
-
--- helper to close nvim-tree if it is the last buffer
--- taken from this discussion:
--- https://github.com/nvim-tree/nvim-tree.lua/issues/1005#issuecomment-1183468091
-
-local modifiedBufs = function(bufs)
-    local t = 0
-    for k,v in pairs(bufs) do
-        if v.name:match("NvimTree_") == nil then
-            t = t + 1
-        end
-    end
-    return t
-end
-
-vim.api.nvim_create_autocmd("BufEnter", {
-    nested = true,
-    callback = function()
-        if #vim.api.nvim_list_wins() == 1 and
-        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
-        modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
-            vim.cmd "quit"
-        end
-    end
-})
-
-
--- Function to handle nvim opening behavior
--- Open the nvim-tree when nvim opens a dir
--- more information here: https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
-
-local function open_nvim_tree(data)
-
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
-
-  if not directory then
-    return
-  end
-
-  -- change to the directory
-  vim.cmd.cd(data.file)
-
-  -- open the tree
-  require("nvim-tree.api").tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
 -- get help with :help nvim-tree.setup
 -- help for specific options help: nvim-tree.option
 
@@ -82,5 +33,52 @@ return {
           enable = true,
         },
     }
+
+    -- helper to close nvim-tree if it is the last buffer
+    -- taken from this discussion:
+    -- https://github.com/nvim-tree/nvim-tree.lua/issues/1005#issuecomment-1183468091
+
+    local modifiedBufs = function(bufs)
+      local t = 0
+      for k,v in pairs(bufs) do
+        if v.name:match("NvimTree_") == nil then
+          t = t + 1
+        end
+      end
+      return t
+    end
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      nested = true,
+      callback = function()
+        if #vim.api.nvim_list_wins() == 1 and
+          vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
+          modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
+          vim.cmd "quit"
+        end
+      end
+    })
+
+    -- Function to handle nvim opening behavior
+    -- Open the nvim-tree when nvim opens a dir
+    -- more information here: https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
+
+    local function open_nvim_tree(data)
+
+      -- buffer is a directory
+      local directory = vim.fn.isdirectory(data.file) == 1
+
+      if not directory then
+        return
+      end
+
+      -- change to the directory
+      vim.cmd.cd(data.file)
+
+      -- open the tree
+      require("nvim-tree.api").tree.open()
+    end
+
+    vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
   end,
 }
